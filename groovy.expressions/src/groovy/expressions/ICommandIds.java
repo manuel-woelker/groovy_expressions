e06c02690f046f8cc305060490d7aac6dad2e8bb
Manuel Woelker <manuel.woelker+github@gmail.com>
2009-07-14 08:55:56 +0200

    Initial commit (RCP Mail sample)

---
+++ groovy.expressions/src/groovy/expressions/ICommandIds.java
@@ -0,0 +1,15 @@
+package groovy.expressions;
+
+/**
+ * Interface defining the application's command IDs.
+ * Key bindings can be defined for specific commands.
+ * To associate an action with a command, use IAction.setActionDefinitionId(commandId).
+ *
+ * @see org.eclipse.jface.action.IAction#setActionDefinitionId(String)
+ */
+public interface ICommandIds {
+
+    public static final String CMD_OPEN = "groovy.expressions.open";
+    public static final String CMD_OPEN_MESSAGE = "groovy.expressions.openMessage";
+    
+}
